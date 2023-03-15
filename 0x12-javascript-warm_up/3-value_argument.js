599216daed6249bdd67f01c64884ece9424c5890
Kuyik2 <kbzoe2@gmail.com>
2023-03-14 21:52:04 -0700

    task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-/*
-   A js script that only prints the first argument passed to it on the
-   Terminal.
-   Commandline arguments in js is made possible using the process.argv
-   which is an array that contains all arguments of the CLI.
-*/
-const value = process.argv[2];
-if (value === undefined) {
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
     console.log('No argument');
 } else {
-    console.log(value);
+    console.log(args[0]);
 }
