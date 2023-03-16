b3e30fe6f21ffc95aa6a6f22cec4a93ed27c3672
Kuyik2 <kbzoe2@gmail.com>
2023-03-16 07:36:23 -0700

    task

--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -7,6 +7,20 @@ console.log(myConverter(2));
 console.log(myConverter(12));
 console.log(myConverter(89));
 
+
+myConverter = converter(16);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));#!/usr/bin/node
+const converter = require('./10-converter').converter;
+
+let myConverter = converter(10);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
 myConverter = converter(16);
 
 console.log(myConverter(2));
