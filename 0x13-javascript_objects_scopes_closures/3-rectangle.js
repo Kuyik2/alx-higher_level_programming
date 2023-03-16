427c633f2d1aa1708671d9f373c99588ed37f4aa
Kuyik2 <kbzoe2@gmail.com>
2023-03-16 07:12:37 -0700

    task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (!(w <= 0 || h <= 0 || !w || !h)) {
-	    this.width = w;
-	    this.height = h;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-	    for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-	    }
-	    console.log('');
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-}
-
-module.exports = Rectangle;
+};
