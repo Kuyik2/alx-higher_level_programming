690d6f9767883ef32acd4953091b99d1ede2c69a
Kuyik2 <kbzoe2@gmail.com>
2023-03-16 07:39:47 -0700

    task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
